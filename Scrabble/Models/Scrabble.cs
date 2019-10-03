245323180562595cc37bb6c491e2b62478df8b5f
Erin Deuel, Jennifer Batara and sofia giordano <erin@>
2019-10-03 10:58:42 -0700

    change first test to include a regex

--- Scrabble/Models/Scrabble.cs
+++ Scrabble/Models/Scrabble.cs
@@ -1,16 +1,34 @@
 // BACKEND CODE GOES IN HERE
 using System;
-
+using System.Text.RegularExpressions;
 
 
 namespace Scrabble
 {
     public class Game
     {
-          public static bool CheckIfLetter(string userInput)
+        // public string Letter {get; set;}
+        // public int LetterPoint {get; set;}
+
+        public static bool CheckIfLetter(string userInput)
+        {
+            Regex reg = new Regex(@"[a-zA-Z]");
+            if(reg.IsMatch(userInput))
             {
+                // ScrabbleScore();
+                return true;
+            }
+            else 
+            {
+                Console.WriteLine("PLease enter a letter or a word.");
                 return false;
             }
+            
+            
+            // char[] userArray = userInput.ToCharArray();
+            // Console.WriteLine(userArray);
+        }
+
     }
  
 }
--- Scrabble/Program.cs
+++ Scrabble/Program.cs
@@ -1,12 +1,12 @@
 // FRONT END CODE
-
+using System;
+using Scrabble;
 public class Program
 {
     public static void Main()
     {
-
+        Console.WriteLine("Some text prompting the User.");
+        // string UserInput = Console.ReadLine();
+        Game.CheckIfLetter("array");
     }
-
-
-
 }
